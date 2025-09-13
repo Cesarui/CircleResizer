0afd3b3de69d179200f64c65600a7ac59a2155e8
Cesar <cpimentelortiz23@gmail.com>
2025-09-13 18:31:06 -0400

    Testing equals method logic of ResizableCricle class

--- src/Tester.java
+++ src/Tester.java
@@ -4,19 +4,17 @@ import org.junit.Test;
 import javax.swing.*;
 
 /**
- * The tester class, to make sure GUI is working correctly.
+ * The tester class, to make sure GUI and all logic are working correctly.
  */
 public class Tester {
     @Test
     public void test() {
 
-        JFrame frame = new JFrame("Resizable Circle");
-        TestResizableCircle panel = new TestResizableCircle();
+        ResizableCircle circle1 = new ResizableCircle(5.0);
+        ResizableCircle circle2 = new ResizableCircle(5.0);
+        System.out.println(circle1.equals(circle2));
+        String text = "blah blah";
 
-        frame.add(panel);
-        frame.setSize(600,400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.setVisible(true);
+        System.out.println(circle1.equals(text));
     }
 }
